71da39f5cc83bc0cbf4c9499bcdf7e810a798fe1
dominik.rohanek <dominik.rohanek@eucanet.cz>
2023-12-05 08:35:05 +0000

    accounts

--- adresnimista/urls.py
+++ adresnimista/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("adresnimista.mista.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
 if settings.DEBUG:
