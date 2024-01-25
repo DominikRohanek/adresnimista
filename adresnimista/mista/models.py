0c6c9a0b4068f80bd6d0858c0e1b5da8b994f18a
dominik.rohanek <dominik.rohanek@eucanet.cz>
2024-01-25 10:00:14 +0000

    commit

--- adresnimista/mista/models.py
+++ adresnimista/mista/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Place(models.Model):
+    identifier = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return self.identifier
+
+
--- adresnimista/mista/views.py
+++ adresnimista/mista/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .forms import PlaceForm
+from .models import Place
 
 def index(request):
-    return render(request,"index.html")
-
+    places=Place.objects.filter(user_id=request.user.id)
+    return render(request,"index.html",{"places":places})
+    
 def registration(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
@@ -14,3 +20,22 @@ def registration(request):
     else:
         form = UserCreationForm()
     return render(request, "registration/registration.html", {"form": form})
+
+@login_required   
+def add_item(request):
+    if request.method == "POST":
+        form = PlaceForm(request.POST)
+
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            messages.add_message(request, messages.INFO, "Místo bylo přidáno")
+            form.save()
+            return redirect("index")
+
+    else:
+
+        form = PlaceForm()
+    return render(request, "add_item.html",{"form": form})
+
