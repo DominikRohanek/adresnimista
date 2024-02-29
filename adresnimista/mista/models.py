b8285104b52f08259d1016979d48c77797b55959
dominik.rohanek <dominik.rohanek@eucanet.cz>
2024-02-29 11:03:59 +0000

    models

--- adresnimista/mista/models.py
+++ adresnimista/mista/models.py
@@ -8,4 +8,23 @@ class Place(models.Model):
     def __str__(self):
         return self.identifier
 
+class City(models.Model):
+    name = models.CharField(max_length=40)
+    postalcode = models.CharField(max_length=10)
 
+    def __str__(self):
+        return f"{self.name} ({self.postalcode})" 
+
+class Address(models.Model):
+
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True)
+    street = models.CharField(max_length=50)
+    orientation_number = models.CharField(max_length=10)
+    descriptive_number = models.CharField(max_length=10)
+    registration_number = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.street} ({self.orientation_number})" 
+
+
+        
